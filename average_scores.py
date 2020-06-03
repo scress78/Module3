4ef29d50ef9f4ca940b65b2e4f03c132bed65a40
scress78 <65985833+scress78@users.noreply.github.com>
2020-06-02 23:06:02 -0500

    Add files via upload

--- average_scores.py
+++ average_scores.py
@@ -1,13 +1,16 @@
 def average():
-    score1 = int(input("Input score one "))
-    score2 = int(input("Input score two "))
-    score3 = int(input("Input score three "))
-    average_score = int((score1+score2+score3)/3)
-    return average_score
+    score1 = int(input("Input score one: "))
+    score2 = int(input("Input score two: "))
+    score3 = int(input("Input score three: "))
+    student_average = int((score1+score2+score3)/3)
+    return student_average
 
 
-
+last_name = input("Input Last Name: ")
+first_name = input("Input First Name: ")
+age = int(input("Input Age: "))
 
 if __name__ == '__main__':
-    print(average())
+    average_score = average()
 
+print(f'{last_name}, {first_name} Age: {age} Average Grade: {average_score:.2f}')
